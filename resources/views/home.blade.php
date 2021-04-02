5bb4bd6d892976b1edfc8df988b542bfd7c5258c
Bladi C. Duran <bladi.duran@gmail.com>
2021-04-02 13:26:26 -0400

    changed home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,251 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <title>Laravel</title>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <!-- Fonts -->
+        <link rel="stylesheet" href="/css/app.css">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+        <!-- Styles -->
+        <!-- <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style> -->
+    </head>
+    <body>
+    <section id="app-layout">
+    @include('includes.side-menu')
+    <div class="welcome-jumbo">
+      <div class="status">NEW</div>
+      <h1>Billy Burger</h1>
+      <img src="https://i.ya-webdesign.com/images/hamburger-and-fries-png-4.png" class="burger-fries">
     </div>
-</div>
-@endsection
+  </section>
+  <section id="information">
+    <div class="restaurant-image">
+      <img src="img/top-down.jpg">
+    </div>
+    <div class="info">
+      <h2>We Started From The Bottom In 1987</h2>
+      <div class="paragraphs">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid dolore, error minus ad officia
+          fugit iusto repellat, explicabo reiciendis inventore mollitia non, libero perspiciatis dicta eius consequatur
+          et
+          similique!Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid dolore, error minus ad
+          officia
+          fugit iusto repellat, explicabo reiciendis inventore mollitia non, libero perspiciatis dicta eius consequatur
+          et
+          similique!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid dolore, error minus ad officia
+          fugit iusto repellat, explicabo reiciendis inventore mollitia non, libero perspiciatis dicta eius consequatur
+          et
+          similique!Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid dolore, error minus ad
+          officia
+          fugit iusto repellat, explicabo reiciendis inventore mollitia non, libero perspiciatis dicta eius consequatur
+          et
+          similique!</p>
+      </div>
+      <a href="#" class="about-link">
+        <span>Learn more about restaurants</span>
+        <div class="circle-right"><i class="fa fa-chevron-right" aria-hidden="true"></i></div>
+      </a>
+    </div>
+  </section>
+  <section id="food-preview">
+    <h2>We have everything you need to kill your hunger</h2>
+    <div class="button-rounded">View Our Menu</div>
+    <div class="container">
+      <div class="left-btn">
+        <i class="fa fa-chevron-left" aria-hidden="true"></i>
+      </div>
+      <div class="right-btn">
+        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+      </div>
+      <div class="food-slider">
+        <div class="sliding-system">
+          <div class="slide">
+            <div class="background"></div>
+            <div class="content">
+              <div class="food-title">
+                Starters
+              </div>
+              <p class="food-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                architecto
+                officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                repellendus natus nihil?
+              </p>
+              <div class="food-image">
+                <img src="https://taco.sfo2.digitaloceanspaces.com/2019/04/CHIPS.png">
+              </div>
+            </div>
+
+          </div>
+          <div class="slide">
+            <div class="background"></div>
+            <div class="content">
+              <div class="food-title">
+                Burgers
+              </div>
+              <p class="food-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                architecto
+                officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                repellendus natus nihil?
+              </p>
+              <div class="food-image">
+                <img src="https://i.ya-webdesign.com/images/hamburger-and-fries-png-4.png">
+              </div>
+            </div>
+
+          </div>
+          <div class="slide">
+            <div class="background"></div>
+            <div class="content">
+              <div class="food-title">
+                Entrees
+              </div>
+              <p class="food-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                architecto
+                officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                repellendus natus nihil?
+              </p>
+              <div class="food-image">
+                <img src="https://www.pngkey.com/png/full/342-3422633_pork-entrees-steak-pork-png.png">
+              </div>
+            </div>
+
+          </div>
+          <div class="slide">
+            <div class="background"></div>
+            <div class="content">
+              <div class="food-title">
+                Sides
+              </div>
+              <p class="food-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                architecto
+                officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                repellendus natus nihil?
+              </p>
+              <div class="food-image">
+                <img src="https://pngriver.com/wp-content/uploads/2018/04/Download-Salad-Transparent-PNG.png">
+              </div>
+            </div>
+
+          </div>
+          <div class="slide">
+            <div class="background"></div>
+            <div class="content">
+              <div class="food-title">
+                Deserts
+              </div>
+              <p class="food-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                architecto
+                officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                repellendus natus nihil?
+              </p>
+              <div class="food-image">
+                <img
+                  src="https://pngriver.com/wp-content/uploads/2018/04/Download-Cupcake-PNG-Transparent-Image-420x190.png">
+              </div>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="background"></div>
+            <div class="content">
+              <div class="food-title">
+                Beers
+              </div>
+              <p class="food-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                architecto
+                officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                repellendus natus nihil?
+              </p>
+              <div class="food-image">
+                <img
+                  src="https://pngriver.com/wp-content/uploads/2018/04/Download-Cupcake-PNG-Transparent-Image-420x190.png">
+              </div>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="background"></div>
+            <div class="content">
+              <div class="food-title">
+                Drinks
+              </div>
+              <p class="food-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi omnis iste ab deleniti cupiditate
+                architecto
+                officia aspernatur nulla iusto delectus doloremque possimus recusandae, reiciendis aliquam vel voluptas
+                repellendus natus nihil?
+              </p>
+              <div class="food-image">
+                <img
+                  src="https://pngriver.com/wp-content/uploads/2018/04/Download-Cupcake-PNG-Transparent-Image-420x190.png">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </body>
+</html>
